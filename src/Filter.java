37557f45891d025e9d6ebd29133343caad78e584
BabisDros <150071850+BabisDros@users.noreply.github.com>
2025-04-11 14:46:32 +0300

    feat: add proper @serial annotation

--- src/Filter.java
+++ src/Filter.java
@@ -1,8 +1,10 @@
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class Filter implements Serializable
 {
+	@Serial
 	private static final long serialVersionUID = 1L;
 
 	double latitude;
