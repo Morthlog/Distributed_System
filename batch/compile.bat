36e4a986df1122348adbfe268ad33bac7b8eae6a
BabisDros <150071850+BabisDros@users.noreply.github.com>
2025-04-25 22:32:24 +0300

    chore(complile): safely delete Worker.class and add optional pause

--- batch/compile.bat
+++ batch/compile.bat
@@ -1,4 +1,7 @@
 @echo off
 cd ..\src
-del Worker.class
+if exist Worker.class del Worker.class
 javac  -cp .;jar/json-simple-1.1.1.jar  Worker.java
+
+//uncomment this for the cmd window to stay open
+//pause
